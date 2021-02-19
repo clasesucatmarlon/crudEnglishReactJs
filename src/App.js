b8cc8e8a58fee04a929dc01092ee1a109d11dcdb
clasesucatmarlon <clasesucatmarlon@gmail.com>
2021-02-19 14:39:31 -0500

    Version 1.  Mostrando datos de la API en una tabla

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+
+/* import { Table, TableContainer, TableHead, TableCell, TableBody, TableRow } from '@material-ui/core'; */
+
+const baseUrl = 'https://api-english-verb-reg.herokuapp.com/verb';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+   const [data, setData] = useState([]);
+
+
+   const peticionGet = async () => {
+      await axios.get(baseUrl)
+         .then(response => {
+            /* console.log('data orig: ', response.data.verbs); */
+            setData(response.data.verbs)
+         });
+   }
+
+   useEffect(async () => {
+      await peticionGet();
+      /* console.log('Estado Data: ', data) */
+   }, [])
+
+
+   return (
+      <div className="App">
+
+         <div className="container-fluid mt-5  table-responsive">
+            <table className="table table-secondary table-striped">
+               <thead className="table-dark ">
+                  <tr>
+                     <th scope="col">#</th>
+                     <th scope="col">Word</th>
+                     <th scope="col">Pronunciation</th>
+                     <th scope="col">Simple Past</th>
+                     <th scope="col">Pronunciation</th>
+                     <th scope="col">Participle Past</th>
+                     <th scope="col">Pronunciation</th>
+                     <th scope="col">Translation</th>
+                     <th scope="col">Category</th>
+                  </tr>
+               </thead>
+               <tbody className="">
+                  {data.map((verb, index) => (
+                     <tr key={verb.id}>
+                        <th scope="row">{index + 1}</th>
+                           <td>{verb.engWord}</td>
+                           <td>{verb.engWordListen}</td>
+                           <td>{verb.engWordSimplePas}</td>
+                           <td>{verb.engWordSimplePasListen}</td>
+                           <td>{verb.engWordParticiplePas}</td>
+                           <td>{verb.engWordParticiplePasListen}</td>
+                           <td>{verb.espWord}</td>
+                           <td>{verb.category}</td>
+                     </tr>
+                     )) 
+                  }
+               </tbody>
+            </table>
+         </div>
+
+      </div>
+   );
 }
 
 export default App;
